5e1bb8f6b903a2aab4b742de987e480674cc2917
agl5702 <137901683+agl5702@users.noreply.github.com>
2024-02-13 09:38:37 -0500

    Update Dashboard.jsx

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,8 +1,8 @@
 import {Box, Button, SimpleGrid, Text} from '@chakra-ui/react'
 import { NavLink } from 'react-router-dom'
-import {TorneoList} from '../components/torneos/TorneoList'
-import { TorneoFormPages } from './torneos/TorneoFormPage'
-import Torneo from '../components/torneos/Torneo'
+import {TorneoList} from '../components/torneos/TorneoList.jsx'
+import { TorneoFormPages } from './torneos/TorneoFormPage.jsx'
+import Torneo from '../components/torneos/Torneo.jsx'
 import React from 'react'
 import img1 from '../assets/1.png';
 import img2 from '../assets/2.png';
